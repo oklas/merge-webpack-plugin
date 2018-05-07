5cc07031265d2604722abf9a3f74a959cbffe356
minijus <3633549+minijus@users.noreply.github.com>
2018-05-07 15:11:31 +0200

    tests(): Refactor tests to ease adding new ones

--- test/package.test.js
+++ test/package.test.js
@@ -2,29 +2,80 @@ var fs = require("fs");
 var path = require("path");
 var expect = require("expect");
 var webpack = require("webpack");
+var rimraf = require("rimraf");
+var MergePlugin = require("../index");
 
 var packageDir = path.join(__dirname, "package");
 var distDir = path.join(__dirname, "dist");
 
-describe('Test package', function() {
-  it('package', function(done) {
-    var options = require('./package/webpack.config.js')
-    options.context = packageDir;
-    options.output.path = distDir;
+describe("merge-webpack-plugin", function() {
+  var baseWebpackConfig;
 
-    webpack(options, function(err, stats) {
-      if(err) return done(err);
-      if(stats.hasErrors()) return done(new Error(stats.toString()));
+  beforeEach(function() {
+    baseWebpackConfig = {
+      context: packageDir,
+      entry: "./index",
+      output: {
+        path: distDir,
+        libraryTarget: "umd"
+      }
+    };
+  });
 
-      var bundle = require(path.join(distDir,'main.js'));
-      var result = bundle(distDir);
-      expect(result).toMatch(/^result/);
+  afterEach(function(done) {
+    rimraf(distDir, done);
+  });
+  afterEach(function(done) {
+    rimraf(path.join(packageDir, "index.js"), done);
+  });
 
-      var data = path.join(distDir, result);
-      data = fs.readFileSync(data, "utf-8");
-      data = JSON.parse(data);
-      expect(data.key.a).toBe(1);
-      expect(data.key.b).toBe(2);
+  it("should merge yaml files with additional loader", function(done) {
+    var webpackConfig = Object.assign({}, baseWebpackConfig, {
+      module: {
+        rules: [
+          {
+            test: /\.(yaml)$/i,
+            use: [
+              MergePlugin.loader({ name: "result.[hash].yaml" }),
+              "yaml-loader"
+            ]
+          }
+        ]
+      },
+      plugins: [
+        new MergePlugin({
+          search: "./src/**/*.yaml"
+        })
+      ]
+    });
+
+    var indexJs = `
+    module.exports = {
+      data: require("./src/yaml/data.yaml"),
+      empty: require("./src/yaml/empty.yaml"),
+      target: require("./src/yaml/target.yaml")
+    }`;
+    fs.writeFileSync(path.join(packageDir, "index.js"), indexJs);
+
+    webpack(webpackConfig, function(err, stats) {
+      if (err) return done(err);
+      if (stats.hasErrors()) return done(new Error(stats.toString()));
+
+      var bundle = require(path.join(distDir, "main.js"));
+      expect(bundle.data).toMatch(/^result.\S+.yaml/);
+      expect(bundle.data).toEqual(bundle.empty);
+      expect(bundle.data).toEqual(bundle.target);
+
+      var mergedYAML = JSON.parse(
+        fs.readFileSync(path.join(distDir, bundle.data), "utf-8")
+      );
+      expect(mergedYAML).toEqual({
+        key: {
+          a: 1,
+          b: 2
+        },
+        ru: "Привет!"
+      });
 
       done();
     });
--- test/package/index.js
+++
@@ -1,8 +0,0 @@
-var path = require("path");
-module.exports = function(dir) {
-  var res1, res2;
-  res1 = require("./src/data.yaml");
-  res2 = require("./src/target.yaml");
-  if(res1 !== res2) return '"'+res1+'" not equal "'+res2+'"';
-  return res1;
-};
